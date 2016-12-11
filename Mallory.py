752b266a27723cb87896a871fabbd6f782f4a0a3
Paige <fredepai@umich.edu>
2016-12-11 15:45:19 -0500

    made Mallory a class

--- Mallory.py
+++ Mallory.py
@@ -2,19 +2,24 @@
 import sys
 #intercept m1, m2 from file, a, b publically known
 #make class where p,g member vars, add hash
-def get_constants():
-	a = 1
-	b = 1
-	W = 3
-	return a, b, W
 
-def gen_attack_vars(a, b, g, p):
-	#get m1 from file
-	#r = pow(m1,a) * pow(g,b,p)
-	#z_1 = m1/(pow(r,X))
-	#get m2 from file
-	#if m2 == pow(g,
-	#		c2 = H(z1)
-	#elif m2 == pow(g,).
-	#		c2 = H(z2)
-	return 
+class Mallory: 
+
+	def __init__(self):
+		self.a = 1
+		self.b = 1
+		self.W = 3
+
+	def get_constants():
+		return a, b, W
+	
+	def gen_attack_vars(a, b, g, p):
+		#get m1 from file
+		#r = pow(m1,a) * pow(g,b,p)
+		#z_1 = m1/(pow(r,X))
+		#get m2 from file
+		#if m2 == pow(g,
+		#		c2 = H(z1)
+		#elif m2 == pow(g,).
+		#		c2 = H(z2)
+		return 
