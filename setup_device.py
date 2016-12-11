827edc87c446e498bb16aa9857f244890b2e1d78
Jennifer Dailey <jldailey@umich.edu>
2016-12-11 18:57:00 -0500

    Changed setup_device

--- setup_device.py
+++ setup_device.py
@@ -7,28 +7,28 @@ from prime_generator import *
 
 class setup_device:
 	
-	def __init__(self,attacker):
-            self.generator = get_generator()
-            self.prime = get_prime()
-            
-            self.Y = attacker.get_public_key()
-            self.prev_key = -1
-            self.a, self.b, self.W = attacker.get_constants()
-            self.prg = random.SystemRandom()
+    def __init__(self,attacker):
+        self.generator = get_generator()
+        self.prime = get_prime()
+        
+        self.Y = attacker.get_public_key()
+        self.prev_key = -1
+        self.a, self.b, self.W = attacker.get_constants()
+        self.prg = random.SystemRandom()
 
-	def get_keys(self):
-	    if self.prev_key < 0:
-	            self.c = self.prg.randrange(self.prime-1)+1;
-	            self.prev_key = self.c;
-	            self.m = pow(self.generator, self.c, self.prime);
-	            return self.c, self.m
-	    else:
-	            self.c1 = self.c
-	            self.t = self.prg.randrange(2);
-	            self.z = pow(self.generator,(self.c1-self.W*self.t),self.prime)*pow(self.Y,((-self.a*self.c1-self.b)%(self.prime-1)), self.prime);
-	            H = hashlib.sha256()
-	            H.update(bytes(str(z),'ascii'));
-	            self.c = H.hexdigest();
-	            self.prev_key = self.c;
-	            self.m = pow(self.generator, int(self.c, 16), self.prime);
-	            return self.c, self.m
+    def get_keys(self):
+        if self.prev_key < 0:
+            self.c = self.prg.randrange(self.prime-1)+1;
+            self.prev_key = self.c;
+            self.m = pow(self.generator, self.c, self.prime);
+            return self.c, self.m
+        else:
+            self.c1 = self.c
+            self.t = self.prg.randrange(2);
+            self.z = pow(self.generator,(self.c1-self.W*self.t),self.prime)*pow(self.Y,((-self.a*self.c1-self.b)%(self.prime-1)), self.prime);
+            H = hashlib.sha256()
+            H.update(bytes(str(z),'ascii'));
+            self.c = H.hexdigest();
+            self.prev_key = self.c;
+            self.m = pow(self.generator, int(self.c, 16), self.prime);
+            return self.c, self.m
