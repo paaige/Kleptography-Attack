9478a70cd885fd67c0bc9ca123aaa432f539007b
Jennifer Dailey <jldailey@umich.edu>
2016-12-10 23:14:26 -0500

    Updated Alice and Bob

--- Alice.py
+++ Alice.py
@@ -1,8 +1,26 @@
 # Alice will get her private and public keys using the secure device,
 # and send the public key to Bob.
 
-from secure-device import *
+from secure_device import *
+from prime_generator import *
 
-keys = get_keys()
+class Alice:
 
-print keys
+    def __init__(self):
+        self.keys = get_keys()
+        self.private_key = self.keys[0]
+        self.public_key = self.keys[1]
+        self.shared_key = 0
+        self.prime = get_prime()
+
+    def set_new_key(self):
+        self.keys = get_keys()
+        self.private_key = self.keys[0]
+        self.public_key = self.keys[1]
+
+    def get_public_key(self):
+        return self.public_key
+
+    def set_shared_key(self, className):
+        self.class_public_key = className.get_public_key()
+        self.shared_key = pow(self.class_public_key, self.private_key, self.prime)
---
+++ Bob.py
@@ -0,0 +1,27 @@
+# Bob will get his private and public keys using the setup-device
+# and send the public key to Alice
+
+from setup_device import *
+from prime_generator import *
+
+class Bob:
+
+    def __init__(self):
+        self.setup = device_protocol()
+        self.keys = setup.get_new_keys()
+        self.private_key = self.keys[0]
+        self.public_key = self.keys[1]
+        self.shared_key = 0
+        self.prime = get_prime()
+
+    def set_new_key(self):
+        self.keys = self.get_new_keys()
+        self.private_key = self.keys[0]
+        self.public_key = self.keys[1]
+
+    def get_public_key(self):
+        return self.public_key
+
+    def set_shared_key(self, className):
+        self.class_public_key = className.get_public_key()
+        self.shared_key = pow(self.class_public_key, self.private_key, self.prime)
